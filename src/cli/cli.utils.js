486d365d43db78e6d4aea56f0ab052422ad90c9c
dusixx <113414935+dusixx@users.noreply.github.com>
2025-11-02 05:53:23 +0200

    refactor: update cli

---
+++ src/cli/cli.utils.js
@@ -0,0 +1,19 @@
+import { styleText as style } from 'util';
+
+export const DefaultProps = {
+  Prompt: '# ',
+  Username: 'anonymous',
+  ExitCmd: '.exit',
+}
+export const salutation = (username = DefaultProps.Username) => {
+  return style("cyan", `\nWelcome to the File Manager, ${username}`
+    + `\nType "${DefaultProps.ExitCmd}" or use "CTRL+C" to finish`
+  );
+}
+export const farewell = (username = DefaultProps.Username) => {
+  return style("cyan", `Thank you for using File Manager, ${username}, goodbye!`
+  );
+}
+export const currentlyIn = (workingDir) => {
+  return style("gray", `You are currently in `) + workingDir;
+}
